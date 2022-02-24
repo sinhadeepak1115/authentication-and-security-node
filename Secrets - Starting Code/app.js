0d317a506240ea1f44bf351c1d86dc8492e88b2c
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-02-24 21:44:46 +0530

    encryption level 1.

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -47,6 +47,23 @@ app.post("/register", function(req, res){
     });
 })
 
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username, password)
+    User.findOne({email: username}, function(err, foundUser){
+        if (err){
+            console.log(err);
+        }else{
+            if (foundUser){
+                if (foundUser === password){
+                    res.render("home");
+                }    
+            }
+        }
+    })
+})
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
