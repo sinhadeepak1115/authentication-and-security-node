01558f0e83329df6063bd9be7f3fd9b31ee3ddbf
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-02-26 01:08:02 +0530

    update app.js

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -30,7 +30,8 @@ mongoose.connect("mongodb://localhost:27017/userDB")
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String 
+    password: String,
+    googleId: String 
 });
 
 userSchema.plugin(passportLocalMongoose); 
@@ -40,8 +41,14 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err, user);
+    });
+});
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
@@ -49,6 +56,7 @@ passport.use(new GoogleStrategy({
     callbackURL: "http://localhost:3000/auth/google/secrets"
     },
     function(accessToken, refreshToken, profile, cb) {
+        console.log(profile);
         User.findOrCreate({ googleId: profile.id }, function (err, user) {
             return cb(err, user);
         });
@@ -61,6 +69,13 @@ app.get("/", function(req, res){
 
 app.get('/auth/google',
     passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+passport.authenticate('google', { failureRedirect: '/login' }),
+function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+});
 
 app.get("/login", function(req, res){
     res.render("login");
