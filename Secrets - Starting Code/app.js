757d7b4bf67d218fc1af368b4053c1cb23ef1064
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-02-25 23:49:39 +0530

    added google auth in app.js

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -6,8 +6,8 @@ const mongoose = require("mongoose");
 const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose  = require('passport-local-mongoose')
-
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate")
 
 const app = express();
 
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.plugin(passportLocalMongoose); 
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
@@ -42,9 +43,24 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }));
 
 app.get("/login", function(req, res){
     res.render("login");
