2d8648f60408f5aedacdbe8b71ca44c1f810eac1
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-02-25 21:21:41 +0530

    update login and logut using passport.

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -62,6 +62,11 @@ app.get("/secrets", function(req, res){
     }
 })
 
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
 app.post("/register", function(req, res){
     User.register({username: req.body.username}, req.body.password , function(err, user) {
         if (err) {
@@ -76,6 +81,21 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+    req.login(user, function(err) {
+        if (err){ 
+            complete.log(err); 
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+            })
+        }
+        
+    });
+
 })
 
 app.listen(3000, function() {
